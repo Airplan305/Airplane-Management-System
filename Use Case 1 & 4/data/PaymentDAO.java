aacdc11e8f85fb9206950083cc6efbceb3e5db1a
Adnan <qassimhassan7@gmail.com>
2024-04-17 15:49:07 +0300

    Use Case 1 & 4 are implemented - Adnan

---
+++ Use Case 1 & 4/data/PaymentDAO.java
@@ -0,0 +1,28 @@
+package data;
+import business.Payment;
+/**
+ * Adnan Mohammed 202008997 sec 1
+ * The PaymentDAO class handles the data access operations related to payments.
+ */
+public class PaymentDAO {
+
+    /**
+     * Retrieves a payment from the database.
+     * @param paymentId the ID of the payment to retrieve.
+     * @return the Payment object if found, null otherwise.
+     */
+    public Payment getPayment(String paymentId) {
+        // Implementation depends on the database setup
+        return null;
+    }
+
+    /**
+     * Saves a payment to the database.
+     * @param payment the Payment object to save.
+     * @return true if the operation is successful, false otherwise.
+     */
+    public boolean savePayment(Payment payment) {
+        // Implementation depends on the database setup
+        return false;
+    }
+}
